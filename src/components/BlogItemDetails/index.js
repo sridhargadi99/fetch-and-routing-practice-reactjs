7c7d2fad01845b9f7bac0197d9dd45e9de6e15de
sridhar <sridhar.gadi99@gmail.com>
2024-02-13 12:45:56 +0000

    fetch and routing practice solution

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,63 @@
+// Write your JS code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {
+    newData: {},
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getBlogDetails()
+  }
+
+  getBlogDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updatedData = {
+      title: data.title,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+      content: data.content,
+    }
+    this.setState({newData: updatedData, isLoading: false})
+  }
+
+  renderBlogItemDetails = () => {
+    const {newData, isLoading} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = newData
+    return isLoading ? (
+      <div data-testid="loader">
+        <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+      </div>
+    ) : (
+      <div className="blog-info">
+        <h1 className="blog-details-title">{title}</h1>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
